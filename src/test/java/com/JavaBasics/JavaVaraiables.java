ad5c118bb873c66ad38b3bf8c351cd98460aa892
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-07-05 13:11:05 +0530

    java variables 05 july 2022

---
+++ src/test/java/com/JavaBasics/JavaVaraiables.java
@@ -0,0 +1,45 @@
+package com.JavaBasics;
+
+public class JavaVaraiables {
+
+	int z = 10;// instance variable of type integer declared outside a method and inside class
+	static String str = "learningJava";// static variable of type String
+	static char ch = 'h';// static variable of type character
+	static int j = 7;// static variable of type integer
+	
+	//for single line comments
+	
+	/*
+	 * i am about 
+	 * commit the
+	 * multiple line
+	 */
+
+	public static void printStatic() {
+		System.out.println("Value of String valirable declared as static:" + str);
+		System.out.println("Value of char valirable declared as static:" + ch);
+	}
+
+	public void sum(int a, int b, int c) {
+		int Result = a + b + c;//local varaible of type Integer
+		System.out.println("Result:" + Result);
+	}
+
+	public void sub(int a, int b) {
+		int ResultOfSub = a - b;//local varaible of type Integer
+		System.out.println("ResultOfSub:" + ResultOfSub);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JavaVaraiables obj = new JavaVaraiables();
+		obj.sum(3, 4, 5);
+		obj.sub(5, 1);
+		obj.z = 9;
+		System.out.println("z:" + obj.z);
+		printStatic();
+		j++;
+		System.out.println(j);
+	}
+
+}
